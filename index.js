82b5428d1acc8d53741367e1c331aa33e189ae7c
John Olubori David <daveholuborhee@gmail.com>
2018-05-20 00:08:03 +0100

    [Chore 157698784] Create challenge2 routes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send({msg: 'Mesage'}))
+app.route('/users/requests')
+  .get((req, res)=> res.send({'msg': 'I give all requests for user'}))
+  .post((req, res) => res.send({'msg': 'I create a new request'}))
 
-app.listen(3000, () => console.log('Example app listening on port 300!'))
+
+app.route('/users/requests/:requestId')
+  .get((req, res)=> res.send({'msg': 'I give full detail about this request'}))
+  .put((req, res) => res.send({'msg': 'I modify this request'}))
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))

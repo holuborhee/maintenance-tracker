423dded8661c1d873d2f963d2831d6901db281bc
John Olubori David <daveholuborhee@gmail.com>
2018-05-20 10:35:25 +0100

    [Chore 157698969] Finish eslint setup and fix errors flagged

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,61 +1,59 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../lib/index.js';
+import { describe, it } from 'mocha';
+import app from '../lib/index';
 
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
-describe('Routes', () =>  {
+describe('Routes', () => {
   describe('/users/requests', () => {
-  	describe('GET', () => {
-  	  it('should return return status of 200', (done) => {
-  	    chai.request(app)
-  	      .get('/users/requests')
-  	      .end((err, res) => {
-  	      	expect(res).to.have.status(200);
-  	      	done();
-  	      })
-  	  });
-  	})
+    describe('GET', () => {
+      it('should return return status of 200', (done) => {
+        chai.request(app)
+          .get('/users/requests')
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+    });
 
-  	describe('POST', () => {
-  	  it('should return return status of 200', (done) => {
-  	    chai.request(app)
-  	      .post('/users/requests')
-  	      .end((err, res) => {
-  	      	expect(res).to.have.status(201);
-  	      	done();
-  	      })
-  	  });
-  	})
-    
+    describe('POST', () => {
+      it('should return return status of 200', (done) => {
+        chai.request(app)
+          .post('/users/requests')
+          .end((err, res) => {
+            expect(res).to.have.status(201);
+            done();
+          });
+      });
+    });
   });
 
 
   describe('/users/requests/:requestId', () => {
-  	describe('GET', () => {
-  	  it('should return return status of 200', (done) => {
-  	    chai.request(app)
-  	      .get('/users/requests/1')
-  	      .end((err, res) => {
-  	      	expect(res).to.have.status(200);
-  	      	done();
-  	      })
-  	  });
-  	})
+    describe('GET', () => {
+      it('should return return status of 200', (done) => {
+        chai.request(app)
+          .get('/users/requests/1')
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+    });
 
-  	describe('PUT', () => {
-  	  it('should return return status of 200', (done) => {
-  	    chai.request(app)
-  	      .put('/users/requests/2')
-  	      .end((err, res) => {
-  	      	expect(res).to.have.status(200);
-  	      	done();
-  	      })
-  	  });
-  	})
-    
+    describe('PUT', () => {
+      it('should return return status of 200', (done) => {
+        chai.request(app)
+          .put('/users/requests/2')
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+    });
   });
-
 });

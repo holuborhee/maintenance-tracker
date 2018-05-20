0d897e45b634eea0eb75d6c7868bc388f0b24323
John Olubori David <daveholuborhee@gmail.com>
2018-05-20 01:15:15 +0100

    [Chore 157698784] Add 404 for no matching request on server

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ app.route('/users/requests/:requestId')
   .get((req, res)=> res.send({'msg': 'I give full detail about this request'}))
   .put((req, res) => res.send({'msg': 'I modify this request'}))
 
+app.use(function (req, res, next) {
+  res.status(404).send({'msg': 'This request does not match any on this server'})
+})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
